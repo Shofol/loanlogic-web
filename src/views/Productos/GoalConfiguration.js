269c495c43a26ec86c1195cce75b9f8c54bb0b9a
shofol <jahananower@gmail.com>
2023-08-05 01:11:44 +0600

    add goal config

--- src/views/Productos/GoalConfiguration.js
+++ src/views/Productos/GoalConfiguration.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
+  Button,
   Card,
   CardBody,
   CardHeader,
@@ -9,24 +10,101 @@ import {
 } from "reactstrap";
 import AsesoresForm from "../Reportería/AsesoresForm";
 import { agenciasValues } from "../../configs/data";
+import api from "../../@core/api/api";
+import { Save } from "react-feather";
+import { toast } from "react-hot-toast";
+import { formatMessage } from "../../utility/functions/formatMessage";
 
 const GoalConfiguration = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchGoals();
+  }, []);
+
+  const fetchGoals = async () => {
+    const response = await api.get("/goal");
+    setData(response.data.data);
+  };
+
+  const submitData = () => {
+    const response = api.post("/goal", data);
+    toast.promise(
+      response,
+      {
+        loading: "Loading",
+        success: (data) => {
+          return `${data.data.message}`;
+        },
+        error: (err) => {
+          return `ERROR: ${formatMessage(err)}`;
+        }
+      },
+      {
+        style: { minWidth: "250px", fontWeight: "bold" }
+      }
+    );
+  };
+
   return (
     <Card>
       <CardHeader>
         <CardTitle tag="h4">Configurar metas</CardTitle>
       </CardHeader>
       <CardBody>
-        <h4>Asesores</h4>
-        {agenciasValues.map((agency) => {
-          return <AsesoresForm key={agency.label} name={agency.label} />;
-        })}
+        <h4 className="ps-2">Asesores</h4>
+        {data &&
+          data
+            .filter((item) => item.role === "AGENT")
+            .map((agencyGoals, index) => {
+              return (
+                <AsesoresForm
+                  key={index}
+                  agency={agencyGoals}
+                  handleChange={(value, goalIndex, type) => {
+                    data.filter(
+                      (item) =>
+                        item.agency === agencyGoals.agency &&
+                        item.role === agencyGoals.role
+                    )[0].goals[goalIndex][type] = value;
+                  }}
+                />
+              );
+            })}
 
-        <h4 className="py-2">Agentes cobro</h4>
+        <h4 className="py-2 ps-2">Agentes cobro</h4>
+        {data &&
+          data
+            .filter((item) => item.role === "COLLECTION-MANAGER")
+            .map((agencyGoals, index) => {
+              return (
+                <AsesoresForm
+                  key={index}
+                  agency={agencyGoals}
+                  handleChange={(value, goalIndex, type) => {
+                    data.filter(
+                      (item) =>
+                        item.agency === agencyGoals.agency &&
+                        item.role === agencyGoals.role
+                    )[0].goals[goalIndex][type] = value;
+                  }}
+                />
+              );
+            })}
 
-        {agenciasValues.map((agency) => {
-          return <AsesoresForm key={agency.label} name={agency.label} />;
-        })}
+        <div className="d-flex justify-content-end me-1">
+          <Button.Ripple
+            className="me-1"
+            color="primary"
+            type="submit"
+            onClick={() => {
+              submitData();
+            }}
+          >
+            <Save size={16} />
+            <span className="align-middle mx-25">Guardar</span>
+          </Button.Ripple>
+        </div>
       </CardBody>
     </Card>
   );
--- src/views/Reportería/AsesoresForm.js
+++ src/views/Reportería/AsesoresForm.js
@@ -1,111 +1,60 @@
 import React from "react";
 import { Input, Label, Row, Col, InputGroup, InputGroupText } from "reactstrap";
-// ** Third Party Components
-import Cleave from "cleave.js/react";
 
-const AsesoresForm = ({ name }) => {
+const AsesoresForm = ({ agency, handleChange }) => {
   const options = { numeral: true };
 
   return (
     <Row className="border m-2">
-      <Col className="offset-4" md="4">
-        <p>Diario</p>
-      </Col>
-      <Col md="4">
-        <p>Mensual</p>
+      <Col md="2" className="fs-5 mt-1 fw-bold d-flex align-items-center">
+        {agency.agency}
       </Col>
 
-      <Col md="2" className="offset-2">
-        <Label className="fs-5">Novato</Label>
-      </Col>
-      <Col className="mb-1" md="4">
-        <InputGroup>
-          <Cleave
-            className="form-control"
-            placeholder="Nombre del producto"
-            options={options}
-            id="numeral-formatting"
-          />
+      <Col md="10" className="mt-1">
+        <Row>
+          <Col md="4" className="offset-2">
+            <p>Diario</p>
+          </Col>
+          <Col md="4">
+            <p>Mensual</p>
+          </Col>
+        </Row>
 
-          {/* <Input
-            type="text"
-            name="product_name"
-            id="product_name"
-            placeholder="Nombre del producto"
-            //   tag={Field}
-          /> */}
-          <InputGroupText>Q</InputGroupText>
-        </InputGroup>
-      </Col>
-      <Col className="mb-1" md="4">
-        <InputGroup>
-          <Cleave
-            className="form-control"
-            placeholder="Nombre del producto"
-            options={options}
-            id="numeral-formatting"
-          />
+        {agency.goals.map((goal, index) => {
+          return (
+            <Row key={index}>
+              <Col md="2">
+                <Label className="fs-5">{goal.category}</Label>
+              </Col>
+              <Col className="mb-1" md="4">
+                <InputGroup>
+                  <Input
+                    type="number"
+                    className="form-control dark-ph"
+                    placeholder={goal.daily}
+                    onChange={(e) =>
+                      handleChange(e.target.value, index, "daily")
+                    }
+                  />
+                  <InputGroupText>Q</InputGroupText>
+                </InputGroup>
+              </Col>
+              <Col className="mb-1" md="4">
+                <InputGroup>
+                  <Input
+                    className="form-control dark-ph"
+                    placeholder={goal.monthly}
+                    onChange={(e) =>
+                      handleChange(e.target.value, index, "monthly")
+                    }
+                  />
 
-          <InputGroupText>Q</InputGroupText>
-        </InputGroup>
-      </Col>
-
-      <Col md="2" className="fs-5">
-        {name}
-      </Col>
-      <Col md="2">
-        <Label className="fs-5">Intermedio</Label>
-      </Col>
-      <Col className="mb-1" md="4">
-        <InputGroup>
-          <Cleave
-            className="form-control"
-            placeholder="Nombre del producto"
-            options={options}
-            id="numeral-formatting"
-          />
-
-          <InputGroupText>Q</InputGroupText>
-        </InputGroup>
-      </Col>
-      <Col className="mb-1" md="4">
-        <InputGroup>
-          <Cleave
-            className="form-control"
-            placeholder="Nombre del producto"
-            options={options}
-            id="numeral-formatting"
-          />
-
-          <InputGroupText>Q</InputGroupText>
-        </InputGroup>
-      </Col>
-
-      <Col md="2" className="offset-2">
-        <Label className="fs-5">Experto</Label>
-      </Col>
-      <Col className="mb-1" md="4">
-        <InputGroup>
-          <Cleave
-            className="form-control"
-            placeholder="Nombre del producto"
-            options={options}
-            id="numeral-formatting"
-          />
-
-          <InputGroupText>Q</InputGroupText>
-        </InputGroup>
-      </Col>
-      <Col className="mb-1" md="4">
-        <InputGroup>
-          <Cleave
-            className="form-control"
-            placeholder="Nombre del producto"
-            options={options}
-            id="numeral-formatting"
-          />
-          <InputGroupText>Q</InputGroupText>
-        </InputGroup>
+                  <InputGroupText>Q</InputGroupText>
+                </InputGroup>
+              </Col>
+            </Row>
+          );
+        })}
       </Col>
     </Row>
   );
