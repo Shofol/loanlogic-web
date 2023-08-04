05846bf9256f0904f8f4ce95648675c7ba5a9aac
shofol <jahananower@gmail.com>
2023-08-05 01:45:04 +0600

    add disbursement table

--- src/views/Dashboards/SupervisorOficina/créditosPendienteDesembolso.js
+++ src/views/Dashboards/SupervisorOficina/créditosPendienteDesembolso.js
@@ -2,11 +2,30 @@ import { MoreVertical, Edit, Trash, Check, X } from "react-feather";
 import { Table, Row, Col, Button } from "reactstrap";
 import { Globe } from "react-feather";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import api from "../../../@core/api/api";
+import StatusTag from "../../../@core/components/statusTag";
 
 const CréditosPendienteDesembolso = () => {
   const navigate = useNavigate();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const [data, setData] = useState(null);
+
+  // // ** Function to handle Pagination
+  // const handlePagination = (page) => {
+  //   setCurrentPage(page.selected + 1);
+  // };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await api.get(`credit/disbursement/pending`);
+    setData(response.data.data);
+    // setTotalPages(response.data.pagination.totalPages);
+  };
 
   return (
     <>
@@ -36,54 +55,49 @@ const CréditosPendienteDesembolso = () => {
             <th>Dirección</th>
             <th>Municipio</th>
             <th>Monto desembolso</th>
-            <th>Estado</th>
             <th>Desembolso</th>
+            <th>Estado</th>
           </tr>
         </thead>
         <tbody>
-          <tr
-            className="clickable-row"
-            onClick={() => {
-              navigate("/clientes/5");
-            }}
-          >
-            <td>1</td>
-            <td>S1034</td>
-            <td>John</td>
-            <td>Doe</td>
-            <td>Calle de monte toro, 30</td>
-            <td>Samayac</td>
-            <td>Suchitepéquez</td>
-            <td>Pendiente pre-validación</td>
+          {data &&
+            data.map((item, index) => {
+              return (
+                <tr
+                  key={item.client.id}
+                  className="clickable-row"
+                  onClick={() => {
+                    navigate(`/clientes/${item.client.id}`);
+                  }}
+                >
+                  <td>{index + 1}</td>
+                  <td>{item?.credit.id}</td>
+                  <td>{item?.client.name}</td>
+                  <td>{item?.client.surname}</td>
+                  <td>{item?.client.residence_address}</td>
+                  <td>{item?.client.residence_municipality}</td>
+                  <td>{item?.credit.disbursement_amount}</td>
+                  <td>
+                    <StatusTag status={item?.debt_collections.status} />
+                  </td>
 
-            <td>
-              <Button.Ripple
-                className="btn-icon"
-                outline
-                color="primary"
-                onClick={(e) => {
-                  // navigate(`/créditos/garantía/${guarantee.id}`);
-                  navigate(`/desembolso/${data?.id}`);
-                  e.stopPropagation();
-                }}
-              >
-                <Edit size={16} />
-              </Button.Ripple>
-            </td>
-
-            {/* <td
-              className="d-flex gap-1"
-              width={"150px"}
-              style={{ width: "150px" }}
-            >
-              <Button.Ripple className="btn-icon" outline color="danger">
-                <Check size={16} />
-              </Button.Ripple>
-              <Button.Ripple className="btn-icon" outline color="danger">
-                <X size={16} />
-              </Button.Ripple>
-            </td> */}
-          </tr>
+                  <td>
+                    <Button.Ripple
+                      className="btn-icon"
+                      outline
+                      color="primary"
+                      onClick={(e) => {
+                        // navigate(`/créditos/garantía/${guarantee.id}`);
+                        navigate(`/desembolso/${item?.debt_collections.id}`);
+                        e.stopPropagation();
+                      }}
+                    >
+                      <Edit size={16} />
+                    </Button.Ripple>
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </Table>
     </>
