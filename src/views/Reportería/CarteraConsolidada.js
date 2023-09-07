ab42503237d46b3368360876c3a8af79840b157b
shofol <jahananower@gmail.com>
2023-09-07 16:54:07 +0600

    fix color error

--- src/views/Reportería/CarteraConsolidada.js
+++ src/views/Reportería/CarteraConsolidada.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Button, Card, Col, Label, Row, Table } from "reactstrap";
 import Flatpickr from "react-flatpickr";
 import { agenciasValues } from "../../configs/data";
@@ -11,10 +11,27 @@ import { Download } from "react-feather";
 import { UserContext } from "../../utility/context/User";
 import { Spanish } from "flatpickr/dist/l10n/es";
 import { getConvertDateWithTimeZone } from "../../utility/Utils";
+import api from "../../@core/api/api";
 
 const CarteraConsolidada = () => {
   const [picker, setPicker] = useState(getConvertDateWithTimeZone(new Date()));
   const { user } = useContext(UserContext);
+  const [agency, setAgency] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, [agency, picker]);
+
+  const fetchData = async () => {
+    const response = await api.get(
+      `reporting/consolidated-portfolio` +
+        `${picker ? `?date=${formatDateForQuery(picker)}` : ""}` +
+        `${agency ? `&agency=${agency}` : ""}`
+    );
+    setData(response.data.data);
+  };
+
   return (
     <Card className="p-2">
       <Row>
@@ -23,11 +40,11 @@ const CarteraConsolidada = () => {
           <Select
             isClearable={false}
             theme={selectThemeColors}
-            isMulti
             name="colors"
             options={user.agency}
             className="react-select"
             classNamePrefix="select"
+            onChange={(option) => setAgency(option.value)}
           />
         </Col>
 
