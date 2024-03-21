3030b566e960899b780bc0a5a2f001e5f194c51c
Josep Martí <one.neuron@gmail.com>
2024-03-21 13:31:35 +0100

    Restor config.js

---
+++ src/@core/api/config.js
@@ -0,0 +1,3 @@
+//export const baseURL = `https://api.alchilazo.com.gt/api/`;
+export const baseURL = `https://testing-api.alchilazo.com.gt/api/`;
+//export const baseURL = `http://localhost:8000/api/`;
